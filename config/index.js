9128e263c85907b9b48d3d3758a080ab48cfa1c6
theWickedWebDev <syoung@paintnite.com>
2018-02-12 15:29:56 -0500

    Update node env config

--- config/index.js
+++ config/index.js
@@ -11,18 +11,18 @@ let config = {
       url: 'http://localhost:5000/graphql',
     },
   },
-  // production: {
-  //   url: 'http://www.mycoin.store',
-  //   db: {
-  //     host:   'localhost',
-  //     port:   '3306',
-  //     user:     'root',
-  //     pass:     'Dalekini21'
-  //   },
-  //   api: {
-  //     url: 'http://api.mycoin.store/graphql',
-  //   }
-  // }
+  production: {
+    db: {
+      name: 'coins_db',
+      host: 'localhost',
+      port: '3306',
+      user: 'root',
+      pass: 'Bagdasar'
+    },
+    api: {
+      url: 'http://api.mycoin.store/graphql',
+    }
+  }
 };
 
 module.exports = config;
