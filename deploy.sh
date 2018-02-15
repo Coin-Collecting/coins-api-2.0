588c732f84565eab95e00c0f265fd1ce1993989d
theWickedWebDev <syoung@paintnite.com>
2018-02-15 00:31:27 -0500

    Added slack message on deploy

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,5 @@ git pull origin master
 npm i
 
 sudo systemctl restart api
+
+curl -X POST --data "payload={\"text\": \":slack: Deployed API\"}" https://hooks.slack.com/services/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
